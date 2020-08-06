d0628bdc9887f541142c7c4567204cabaf3b1929
Jayson Hernandez <68240645+hernjack@users.noreply.github.com>
2020-08-07 09:34:35 +1000

    Add files via upload

--- Mouse/eventoMouse.js
+++ Mouse/eventoMouse.js
@@ -1,19 +1,34 @@
 var cuadro = document.getElementById("areaDibujo");
 var papel = cuadro.getContext("2d");
+var boton = document.getElementById("b_enviar");
+var alto = document.getElementById("alto_canvas");
+var ancho = document.getElementById("ancho_canvas");
+var c_canvas = document.getElementById("color_canvas");
+var color;
 var x, y, xf, yf;
-var color = "red";
-var ancho = cuadro.width;
 var estado;
 
+
+boton.addEventListener("click", dibujarCanvas);
 cuadro.addEventListener("mousedown", presionarMouse);
 cuadro.addEventListener("mousemove", dibujarMouse);
 cuadro.addEventListener("mouseup", levantarMouse);
 
-dibujarLinea(color, 1, 1, 1, ancho-1, papel);
-dibujarLinea(color, 1, ancho-1, ancho-1, ancho-1, papel);
-dibujarLinea(color, 1, 1, ancho-1, 1, papel);
-dibujarLinea(color, ancho-1, 1, ancho-1, ancho-1, papel);
 
+function dibujarCanvas(evento)
+{
+  cuadro.width = parseInt(ancho.value);
+  cuadro.height = parseInt(alto.value);
+  var w = cuadro.width;
+  var h = cuadro.height;
+  color = c_canvas.value;
+  console.log(color);
+  dibujarLinea(color, 1, 1, w-1, 1, papel);
+  dibujarLinea(color, 1, 1, 1, h-1, papel);
+  dibujarLinea(color, w-1, 1, w-1, h-1, papel);
+  dibujarLinea(color, 1, h-1, w-1, h-1, papel);
+
+}
 
 function presionarMouse(evento)
 {
@@ -29,22 +44,16 @@ function dibujarMouse(evento)
   {
     xf = evento.layerX;
     yf = evento.layerY;
-    console.log(x +" " + y + " " + xf + " " + yf);
     dibujarLinea(color, x, y, xf, yf, papel);
     x = xf;
     y = yf;
-
   }
 }
 
 function levantarMouse(evento)
 {
   estado = false;
-  //x = evento.layerX;
-  //y = evento.layerY;
 }
-
-
 
 function dibujarLinea(color_linea, xinicial, yinicial, xfinal, yfinal, lienzo)
 {
